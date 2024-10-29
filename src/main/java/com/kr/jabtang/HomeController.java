d88616f5aec3ebe7a1919186473129aaa6691a26
dohyun9160 <dohyun9160@gmail.com>
2024-10-29 19:02:38 +0900

    단순 업로드 완료
    - 경로설정한 파일 이 다 있어야함

--- src/main/java/com/kr/jabtang/HomeController.java
+++ src/main/java/com/kr/jabtang/HomeController.java
@@ -1,5 +1,6 @@
 package com.kr.jabtang;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -8,12 +9,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+
 
 
 @Controller
+@Log4j
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -34,6 +42,31 @@ public class HomeController {
 	@RequestMapping(value = "/naver", method = RequestMethod.GET)
 	public String callback(Locale locale, Model model) {
 		return "login/naverCallback";
+	}
+	
+	@GetMapping(value = "/uploadTest")
+	public void uploadTest() {	
+		System.out.println("upload form");
+	}
+	
+	@PostMapping("/uploadAction")
+	public void uploadAction( MultipartFile[] uploadFile, Model model ) {
+	
+		String uploadFolder = "C:\\upload";
+		
+		for( MultipartFile multipartFile : uploadFile ) {
+			System.out.println("-----------------------------------");
+			System.out.println("Upload File Name : " + multipartFile.getOriginalFilename());
+			System.out.println("Upload File Size : " + multipartFile.getSize());
+			
+			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			}catch (Exception e) {
+				System.out.println("/uploadAction 오류 :" + e.getMessage());
+			}
+		}
 	}
 	
 	
